9560475abfb0c09442885732e8c8b34e71da9b7c
Mohit Mittal <mittalmohit2003@gmail.com>
2024-01-22 00:59:41 +0530

    added event listner to each song in library and added play pause functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-async function getsongs() {
+  let currentSong = new Audio();
+  async function getsongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/");
     let response = await a.text();
     let div = document.createElement("div");
@@ -7,50 +8,64 @@ async function getsongs() {
     let songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if (element.href.endsWith(".mp3")) { 
-            songs.push(element.href.split(/songs/)[1])      
-          }
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(/songs/)[1])
+        }
     }
 
     return songs
 }
 
+const playmusic = (track)=>{
+   // let audio = new Audio(track)
+    currentSong.src = "/songs/" + track
+    currentSong.play()
+    play.src = "pause.svg"
+}
 
 
 async function main() {
+  
+    //get the list of all songs
     let songs = await getsongs()
-    console.log(songs)
-    
+    //console.log(songs)
+
     //show all the songs in playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li> <img class="invert" src="music.svg" alt="">
         <div class="info">
-            <div>${song.replaceAll("%20", " ").replaceAll("/", "").replaceAll("%5D", "").replaceAll("%5B", "").replaceAll(".mp3", "")}</div>
-            <div>Song Artist</div>
+        <div>${song.replaceAll("%20", " ").replaceAll("/", "").replaceAll("%5D", "").replaceAll("%5B", "")}</div>
+        <div>Song Artist</div>
         </div>
         <div class="playnow">
             <span>Play Now</span>
             <img class="invert" src="play.svg" alt="">
         </div>
     </li>`;
-        
-        
-    }
+ }
 
-
-    //play the first song
-    var audio = new Audio(songs[0]);
-   /// audio.play();
-
-    
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration;
-        console.log(duration)
-        // The duration variable now holds the duration (in seconds) of the audio clip
-    });
-
+    // atttach an event listner to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playmusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+    })
 }
+
+    // attach event listner to play, next and previous
+    play.addEventListener("click", ()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "play.svg"
+        }
+    })
+
 
 
 main()
